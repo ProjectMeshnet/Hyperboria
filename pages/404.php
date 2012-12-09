1f212223d55574bc1f90ed20649b094df55bf277
Komachi Onozuka <komachi.onozuka@ya.ru>
2012-12-09 14:52:38 +0400

    Orfography fixies, translation mechanism powered by gettext, Russian translation.

--- pages/404.php
+++ pages/404.php
@@ -1,5 +1,14 @@
-
-<h1>Page not found.</h1>
-<p><i>Thats a 404 error, sorry.</i></p>
+<?php
+$locale = "ru_RU.UTF-8";
+$domain = "messages";
+if (isSet($_GET["lang"])) $locale = $_GET["lang"];
+putenv("LANG=".$locale);
+setlocale(LC_MESSAGES, $locale);
+bindtextdomain($domain, "/home/perd/src/Hyperboria/locale");
+bind_textdomain_codeset($domain, 'UTF-8');
+textdomain($domain);
+?>
+<h1><?php echo _("Page not found."); ?></h1>
+<p><i><?php echo _("Thats a 404 error, sorry."); ?></i></p>
 
 <!-- What should the user do? Suggested pages could go here. -->
